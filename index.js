31983c5bb3937c5338612fe828d0198b5548b227
lolapineiro <111918281+CarolinaPi@users.noreply.github.com>
2023-06-13 19:13:37 -0300

    feat: create app habits

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const app = new Vue ({
+    el: "#contenedor",
+    data: {
+        titulo: 'Creando hábitos épicos',
+        message: 'Recuerda que incorporar buenos hábitos lleva tiempo y esfuerzo, pero los beneficios para tu salud y bienestar valen la pena. ¡Empieza hoy mismo a cuidar de ti mismo/a y a cultivar una vida más saludable y equilibrada!'
+    }
+}); 
+
