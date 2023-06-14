a023bd705c8b88b82fe34175bf70390742044805
lolapineiro <111918281+CarolinaPi@users.noreply.github.com>
2023-06-13 21:14:34 -0300

    feat: add google Fonts and title

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const app = new Vue ({
     el: "#contenedor",
     data: {
         titulo: 'Creando hábitos épicos',
+        titulo2: 'Consejos de hoy y de siempre',
         message: 'Recuerda que incorporar buenos hábitos lleva tiempo y esfuerzo, pero los beneficios para tu salud y bienestar valen la pena. ¡Empieza hoy mismo a cuidar de ti mismo/a y a cultivar una vida más saludable y equilibrada!'
     }
 }); 
