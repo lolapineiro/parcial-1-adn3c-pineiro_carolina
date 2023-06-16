7d77c121f5a7325aba9aab34d8aa36d5abdbdb60
lolapineiro <111918281+CarolinaPi@users.noreply.github.com>
2023-06-16 10:22:33 -0300

    fead add footer

---
+++ components/habito-agradecer.js
@@ -0,0 +1,99 @@
+
+Vue.component("habito-agradecer", {
+    data: function () {
+      return {
+        agradecimiento: "",
+        si: true,
+        vacio: true,
+        gracias: [],
+        editGracias: null,
+      };
+    },
+  
+    template: `
+    <div class="container">
+    
+    <div class="d-flex">
+        <input v-model="agradecimiento" class="form-control me-2" type="text" placeholder="Hoy agradezco por..."
+          aria-label="Aprender a decir gracias">
+        <button type="button" class="btn" @click="nuevoGracias"><img src="./images/agregar-boton.png" alt="agregar gracias"></button>
+      </div>
+  
+      <div :class="si ? 'ingresado' : 'noIngresado'">
+        <p class="emphasized">Por favor escriba aqui sus agradecimientos diarios</p>
+      </div>
+  
+      <table class="table mt-4 ">
+        <thead>
+          <tr>
+            <th scope="col">Agradecimientos</th>
+         
+          </tr>
+        </thead>
+  
+        <template v-if="vacio">
+          <p>Agregue sus afirmaciones de hoy</p>
+        </template>
+      <template v-else>
+        <tbody>
+  
+        
+          <tr v-for="(agradecimiento, escrito) in gracias" :key="escrito" class="mb-3">
+            <th scope="row">{{agradecimiento.escritura}}</th>
+  
+            <td>
+              <button type="button" class="btn " @click="eliminar">
+                <img src="./images/eliminar.png" alt="Eliminar"></img>
+              </button>
+            </td>
+  
+            <td>
+              <button type="button" class="btn" @click="editar(escrito)">
+                <img src="./images/lapiz.png"  alt="Editar"></img>
+              </button>
+            </td>
+  
+          </tr>
+        </tbody>
+      </template>
+    </table>
+  </div>    
+          `,
+  
+  
+    methods: {
+      nuevoGracias: function () {
+        if (this.agradecimiento.length === 0) {
+          this.si = false;
+          return;
+        }
+  
+        if (this.edit == null) {
+          this.si = true;
+          this.vacio = false;
+          this.gracias.push({
+            escritura: this.agradecimiento,
+          });
+          this.agradecimiento = "";
+        } else {
+          this.gracias[this.edit].info = this.agradecimiento;
+          this.edit = null;
+          this.agradecimiento = "";
+        }
+      },
+  
+      //Eliminar nota de agradecimiento
+      eliminar: function (escrito) {
+        this.gracias.splice(escrito, 1);
+      },
+  
+      //Editar nota de agradecimiento
+      editar: function (escrito) {
+        this.agradecimiento = this.gracias[escrito].escritura;
+        this.edit = escrito;
+      },
+    },
+  
+   
+  });
+  
---
+++ components/header-component.js
@@ -0,0 +1,14 @@
+Vue.component("header-component", {
+    template: `
+      <div
+              class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border border-ligh">
+  
+              <nav class="nav justify-content-center d-flex align-items-center">
+                  <img class="logo" src="./images/luna_llena.jpg" alt="logo tejiendo ideas">
+                  <a class="nav-link link-dark" href="./index.html" aria-current="page">Diario de gratitud</a>
+                  <a class="nav-link link-dark" href="./consejos.html">Consejos</a>
+  
+              </nav>
+          </div>
+      `,
+  });
