c002e3e04d94f78a8dcc8dc33c7a5c9a34104a14
lolapineiro <111918281+CarolinaPi@users.noreply.github.com>
2023-06-16 10:23:24 -0300

    add

---
+++ components/footer-component.js
@@ -0,0 +1,12 @@
+
+Vue.component("footer-component", {
+    template: `
+      <div 
+      class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
+      <p class="text-center text-success"><span class="emphasized">Carolina Piñeiro</span> <strong>
+          Parcial I - ADM N3C
+      </strong></p>
+      </div > 
+      `,
+  });
+  
